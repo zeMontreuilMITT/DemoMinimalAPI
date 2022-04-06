15205ed1c4eca409d99ee9e1317e6d613ef2cda9
zeMontreuilMITT <zacharie.montreuil@mitt.ca>
2022-04-06 10:55:39 -0500

    Add parameterized GET methods

--- Program.cs
+++ Program.cs
@@ -14,9 +14,18 @@ app.MapGet("/countries", async (WorldContext db) =>
     return await db.Countries.ToListAsync();
 });
 
-app.MapGet("/cities", async (WorldContext db) =>
+app.MapGet("/cities", async (int? population, WorldContext db) =>
 {
+    if(population != null)
+    {
+        return await db.Cities.Where(c => c.Population >= population).ToListAsync();
+    }
     return await db.Cities.ToListAsync();
+});
+
+app.MapGet("/cities/{id}", async (int id, WorldContext db) =>
+{
+    return await db.Cities.FirstAsync(c => c.Id == id);
 });
 
 app.Run();
